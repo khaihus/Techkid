008b4605b04cec7889b66267cc29ab04414c7fd6
kai <khai98yn@gmail.com>
2018-10-08 10:49:27 +0700

    React Hotgirls

---
+++ ReactHotGirl/build/precache-manifest.624e740c6ba76df0093e374ce48331a5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "195bc4996faf30fc6be6",
+    "url": "/static/js/main.195bc499.chunk.js"
+  },
+  {
+    "revision": "ec2a3476a070df752a5e",
+    "url": "/static/js/1.ec2a3476.chunk.js"
+  },
+  {
+    "revision": "195bc4996faf30fc6be6",
+    "url": "/static/css/main.e5d39110.chunk.css"
+  },
+  {
+    "revision": "ec2a3476a070df752a5e",
+    "url": "/static/css/1.524697d7.chunk.css"
+  },
+  {
+    "revision": "aba139549351d881ae166ca458f40163",
+    "url": "/index.html"
+  }
+];
---
+++ ReactHotGirl/src/config.js
@@ -0,0 +1,4 @@
+export default {
+    rootPath: 'http://localhost:6969'
+    // rootPath: process.env.REACT_APP_STATE === "localhost" ? "http://localhost:6969" : ""
+};
